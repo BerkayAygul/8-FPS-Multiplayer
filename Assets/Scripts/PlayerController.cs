f132a9ed788397195f3418555411f096ff13aea3
BerkayAygul <berkaygithub@gmail.com>
2022-10-29 16:19:06 +0300

    Added C# Regions For Code Readability
    - Using C# Regions To Improve Code Readability

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public Transform playerViewPoint;
     public float mouseSensitivity = 1f;
+    #region comment
     // How much rotation we want our viewpoint to have. How much rotation we wanted to have to look up and down.
-    // We can limit how much movement we have up and down.
+    // We can limit how much movement we have up and down. 
+    #endregion
     private float verticalRotationStore;
-    // When we move horizontally, it is the X movement of the mouse and when we move vertically it is the Y movement of the mouse.
+    #region comment
+    // When we move horizontally, it is the X movement of the mouse and when we move vertically it is the Y movement of the mouse. 
+    #endregion
     private Vector2 mouseInput;
     public bool invertLook;
 
@@ -19,24 +23,28 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDirection;
     private Vector3 movement;
 
+    #region comment
     /* A CharacterController allows you to easily do movement constrained by collisions without having to deal with a rigidbody.
-       A CharacterController is not affected by forces and will only move when you call the Move function. 
+       CharacterController is not affected by forces and will only move when you call the Move function. 
        It will then carry out the movement but be constrained by collisions.
        We are going to use this to move our player around instead of the transform.position.
        One other thing about the Character Controller is it allows us to deal with things like slopes very handly.
        Another thing about the Character Controller is it has a limit for the amount of slope (degree) it can handle (can be adjusted).
        45 degrees (default) is a reasonable slope limit that a player is able to walk up.
     */
+    #endregion
     public CharacterController characterController;
 
-    // When the camera updates the player, the head of the player moves slightly and gets a kind of jittery movement effect. 
-
+    #region comment
     /* When the player gets killed, we have no camera displaying on our screen. 
        So the camera is no longer the child object of the player. 
-       We will just tell our camera to move to the view point every frame (will be updated later). */
+       We will just tell our camera to move to the view point every frame (will be updated later). 
+    */
     /* When the player dies, it is going to be deleted from the scene then we are going to create a new player,
        so we don't want to have to assign the camera manually to that player whenever we create a new one, because
-       we won't be able to do that when the game is running. So we are going to create a private camera reference. */
+       we won't be able to do that when the game is running. So we are going to create a private camera reference. 
+    */
+    #endregion
     private Camera camera;
 
     private float yVelocity;
@@ -44,22 +52,31 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float gravityMod = 2.5f;
 
-    // This is going to be the point in space that we want this invisible line to go down.
+    #region comment
+    // This is going to be the point in space that we want this invisible line to go down. 
+    #endregion
     public Transform groundCheckPoint;
-    // We are going to need to store whether we are on the ground or not.
+    #region comment
+    // We are going to need to store whether we are on the ground or not. 
+    #endregion
     private bool isGrounded;
+    #region comment
     // By using UI, we are going to create a new user layer called "Ground_Layer" and assign it to Environment's layer.
-    // Then we are going to assign Ground_Layer to the newly created LayerMask.
+    // Then we are going to assign Ground_Layer to the newly created LayerMask. 
+    #endregion
     public LayerMask groundLayers;
 
     public GameObject bulletImpact;
 
     void Start()
     {
+        #region comment
         // The mouse shouldn't fly around the place where we're moving around. It should only move anywhere within the window.
         // Confined = the cursor can't move outside the window.
         // Locked = the cursor gets locked to the center of the screen and makes the mouse disappear completely.
         // Note: If we press the escape button, the cursor will be free in the editor (not in the final build).
+
+        #endregion        
         Cursor.lockState = CursorLockMode.Locked;
 
         camera = Camera.main;
@@ -69,12 +86,15 @@ public class PlayerController : MonoBehaviour
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
+        #region comment
         // Quarternion.Euler basically means we can treat this as a vector 3 object, so we can just affect it based on the x, y, z.
         // eulerAngles are basically means the vector 3 version of the rotation angles.
         // We do not want to change x and z values. Leave it whatever the transform rotation currently is.
-        // The player will be able to look right and left.
+        // The player will be able to look right and left. 
+        #endregion
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
+        #region comment
         // We do not want to change y and z values. Leave it whatever the transform rotation currently is.
         // The player will be able to look up and down.
         // The view is currently inverted, so we have to fix this by taking the minus value of the verticalRotationStore value.
@@ -83,7 +103,8 @@ public class PlayerController : MonoBehaviour
         // When we look up a little bit, once the x rotation value gets below zero, the rotation steps back to 60. */
         /* We can't directly clamp the x value of playerviewpoint. Behind the scenes of unity and its internal systems, we can
         // see the actual values of rotation by using Debug.log. We have to clamp the value by using verticalRotationStore. */
-        // Flipping the camera should be optional, so we will use a bool value to make it optional.
+        // Flipping the camera should be optional, so we will use a bool value to make it optional. 
+        #endregion
 
         verticalRotationStore = verticalRotationStore + mouseInput.y;
         verticalRotationStore = Mathf.Clamp(verticalRotationStore, -60, 60);
@@ -98,18 +119,22 @@ public class PlayerController : MonoBehaviour
         }
 
 
+        #region comment
         /* Wrong use:
-         * playerViewPoint.rotation = Quaternion.Euler(Mathf.Clamp(playerViewPoint.rotation.eulerAngles.x - mouseInput.y, -60f, 60f)
+           playerViewPoint.rotation = Quaternion.Euler(Mathf.Clamp(playerViewPoint.rotation.eulerAngles.x - mouseInput.y, -60f, 60f)
         */
+        #endregion
 
 
+        #region comment
         // Time.deltaTime is how long it takes for each frame update to happen. 
         // If our game is running at 60 frames a second, this will be one divided by 60.
         /* If we said we could make our move speed much smaller, that would work but at different frame rates, the player would move
         ** at different speeds. */
         /* If we look sideways and press forward, the player will still go in the same direction that we had before, which is wrong.
         ** We are going to use transform.forward and transform.right to fix this. */
-        // We are moving a lot faster when we move in a diagonal action. We can fix that by normalizing the vector.
+        // We are moving a lot faster when we move in a diagonal action. We can fix that by normalizing the vector. 
+        #endregion
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
         if(Input.GetKey(KeyCode.LeftShift))
@@ -121,6 +146,7 @@ public class PlayerController : MonoBehaviour
             activeMoveSpeed = moveSpeed;
         }
 
+        #region comment
 
         // When we start applying gravity to our player, we are going to make some changes to the y value of the movement.    
         // We don't want movement to get multiplied by the value of the y-axis of activeMoveSpeed.
@@ -135,18 +161,24 @@ public class PlayerController : MonoBehaviour
            but you fall faster and faster over time.
            So what we need to do is make sure that the Y value for these isn't reset. 
            We have to store the current Y movement that we have, which will be the velocity of our player.
-           We are going to create a disposable float value that we will call y velocity, before applying any movement. */
+           We are going to create a disposable float value that we will call y velocity, before applying any movement. 
+        */
         /* If we open debug, we can see that y velocity is continually growing faster and faster, we do not want our player
-           constantly falling down to the ground like that. We are going to add an extra check to fix that issue. */
+           constantly falling down to the ground like that. We are going to add an extra check to fix that issue. 
+        */
+        #endregion
         float yVelocity = movement.y;
         movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized * activeMoveSpeed;
         movement.y = yVelocity;
         if(characterController.isGrounded)
         {
-            // y movement should be 0 if the player is on the ground.
+            #region comment
+            // y movement should be 0 if the player is on the ground. 
+            #endregion
             movement.y = 0;
         }
 
+        #region comment
         // Jump
         /* The ‘get button’ functions work similarly to the ‘get key’ functions, except you cannot use KeyCodes, 
            own buttons in the input manager.This is the recommended by Unity and can be quite powerful as it allows 
@@ -166,7 +198,8 @@ public class PlayerController : MonoBehaviour
            third parameter = how long we want it to go (adjust the value if the player can't jump on the edges),
            fourth parameter = the layer we are want to check.
          */
-        
+        #endregion
+
         isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .5f, groundLayers);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
@@ -176,17 +209,23 @@ public class PlayerController : MonoBehaviour
 
         movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
         characterController.Move(movement * Time.deltaTime);
-        //transform.position += movement * moveSpeed * Time.deltaTime;
-        
-        // If the player presses the left mouse button, shoot.
-        if(Input.GetMouseButtonDown(0))
+        #region comment
+        //transform.position += movement * moveSpeed * Time.deltaTime; 
+        #endregion
+
+        #region comment
+        // If the player presses the left mouse button, shoot. 
+        #endregion
+        if (Input.GetMouseButtonDown(0))
         {
             Shoot();
         }
 
 
+        #region comment
         // If the player presses the escape button, free the cursor.
-        // If the player presses the left mouse button, lock the cursor.
+        // If the player presses the left mouse button, lock the cursor. 
+        #endregion
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -200,36 +239,50 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    #region comment
+    // When the camera updates the player, the head of the player moves slightly and gets a kind of jittery movement effect. 
     // We need to make sure that the player has moves before we update the camera.
-    // LateUpdate() basically happens after Update().
+    // LateUpdate() basically happens after Update(). 
+    #endregion
     private void LateUpdate()
     {
         camera.transform.position = playerViewPoint.position;
         camera.transform.rotation = playerViewPoint.rotation;
     }
 
+    #region comment
     /* Think of how fast a bullet moves. Generally speaking, in reality, when you shoot a bullet, you would not see the actual bullet.
        You see the bullet being fired and you see where it hit and both of these things happen almost in the same time.
        That is how most games deal with it, there are no bullets flying through the air.
        The game calculates where the point is that you hit against.
        We are going to do a raycast to implement this idea.
     */
+    #endregion
     private void Shoot()
     {
+        #region comment
         // Pick a point within the camera, go from that point and go straight forward from the direction the camera is facing.
-        // x: .5f, y: .5f, halfway across on the x-axis and halfway up on the y-axis. That will get us the exact center point.
+        // x: .5f, y: .5f, halfway across on the x-axis and halfway up on the y-axis. That will get us the exact center point. 
+        #endregion
         Ray bulletRay = camera.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
-        // The origin point for that ray.
+        #region comment
+        // The origin point for that ray. 
+        #endregion
         bulletRay.origin = camera.transform.position;
 
+        #region comment
         /* Basically, a RaycastHit is the result of the Raycast.
            If the raycast detects something ahead of us when we shoot forward, it will store whatever information it finds in this
            raycastHit object and it will send it back out.
         */
-        if(Physics.Raycast(bulletRay, out RaycastHit raycastHit))
+        #endregion
+        if (Physics.Raycast(bulletRay, out RaycastHit raycastHit))
         {
-            //Debug.Log("Hit - " + raycastHit.collider.gameObject.name);
+            #region comment
+            //Debug.Log("Hit - " + raycastHit.collider.gameObject.name); 
+            #endregion
 
+            #region comment
             // Create a bullet impact when we detect a hit.
             // We want the bullet impact to go wherever we hit.
             /* The rotation we want to have is whatever the surface is that we hit.
@@ -253,6 +306,7 @@ public class PlayerController : MonoBehaviour
             /* If we start adding a whole bunch of the bullet impact objects, we can keep layering them on top of each other and that
                is because when we created them, the quads themselves have a mesh collider attached to them. We need to remove it.
             */
+            #endregion
 
             GameObject bulletImpactObject = Instantiate(bulletImpact, raycastHit.point + (raycastHit.normal * .002f), Quaternion.LookRotation(raycastHit.normal, Vector3.up));
             Destroy(bulletImpactObject, 10f);
